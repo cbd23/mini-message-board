3e3e2256f5be1fba93f2f8408cbb9d3406471de0
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-10-02 17:28:59 +0300

    Add sample messages inside index router.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,19 @@ import { Router } from "express"
 
 const indexRouter = Router()
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];  
+
 indexRouter.get("/", (req, res) => {
     res.render("index")
 })
