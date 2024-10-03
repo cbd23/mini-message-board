492f377415e408ac647f9e076b28e7f0a39f7c1a
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-10-03 15:22:33 +0300

    Import 'messages' arr & add middleware needed to handle POST req.

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,9 +1,19 @@
 import { Router } from "express"
+import { messages } from "./indexRouter.js"
 
 const newRouter = Router()
 
 newRouter.get("/", (req, res) => {
     res.render("new")
+})
+
+newRouter.post("/", (req, res) => {
+    let message = req.body.message
+    let username = req.body.username
+
+    messages.push({text: message, user: username, added: new Date() })
+
+    res.redirect("/")
 })
 
 export default newRouter
