d2834a53c2debb82b95ef228d9b773e50bb948bf
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-10-03 15:19:45 +0300

    Add needed middleware to parse the form data into req.body.

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
